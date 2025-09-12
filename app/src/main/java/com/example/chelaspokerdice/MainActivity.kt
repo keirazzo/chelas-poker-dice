7390557a7cd826c1d6dc868a80323a9171e06e51
keirazzo <zzokeira@gmail.com>
2025-09-12 19:19:37 +0100

    Closes #1 :create TitleScreen page

--- app/src/main/java/com/example/chelaspokerdice/MainActivity.kt
+++ app/src/main/java/com/example/chelaspokerdice/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.chelaspokerdice.ui.TitleScreen
 import com.example.chelaspokerdice.ui.theme.ChelasPokerDiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChelasPokerDiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TitleScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChelasPokerDiceTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/chelaspokerdice/ui/TitleScreen.kt
@@ -0,0 +1,30 @@
+package com.example.chelaspokerdice.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TitleScreen() {
+    Column (modifier = Modifier.fillMaxSize() ){
+        Text("Chelas Poker Dice",
+            fontSize = 60.sp,
+            lineHeight = 1.2.em,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 150.dp))
+    }
+}
+
+@Composable
+@Preview
+fun TitleScreenPreview() {
+    TitleScreen()
+}
